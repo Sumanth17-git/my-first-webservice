608862668d37b5fb979b535ad22790f7d8992545
Sumanth17-git <sumanth.suman17@gmail.com>
2025-03-15 11:42:33 +0530

    java code cahnges

---
+++ my-first-webservice/src/main/java/com/example/oomapplication/MyFirstWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.oomapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstWebserviceApplication.class, args);
+	}
+
+}
